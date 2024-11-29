3f9c918cae82624205e83a0fb7eeca7468301a80
Bertie Cartwright <sunstonesharptor@gmail.com>
2024-11-29 19:21:58 +0000

    Moved the building menu into its own class

--- src/main/java/y111studios/UIInputProcessor.java
+++ src/main/java/y111studios/UIInputProcessor.java
@@ -12,32 +12,24 @@ import y111studios.buildings.premade_variants.VariantProperties;
 import y111studios.utils.MenuTab;
 
 public class UIInputProcessor implements InputProcessor {
-    Viewport viewport;
-    MenuTab[] currentMenuTab;
-    int[] currentMenuItem;
     World world;
     GameState gameState;
-    Map<MenuTab, VariantProperties[]> buildingVariants;
     boolean[] showDebugInfo;
+    BuildingMenu buildingMenu;
 
     UIInputProcessor(
-        Viewport viewport, MenuTab[] currentMenuTab, int[] currentMenuItem, World world,
-        GameState gameState, Map<MenuTab, VariantProperties[]> buildingVariants,
-        boolean[] showDebugInfo
+            BuildingMenu buildingMenu, World world, GameState gameState, boolean[] showDebugInfo
     ) {
-        this.viewport = viewport;
-        this.currentMenuTab = currentMenuTab;
-        this.currentMenuItem = currentMenuItem;
+        this.buildingMenu = buildingMenu;
         this.world = world;
         this.gameState = gameState;
-        this.buildingVariants = buildingVariants;
         this.showDebugInfo = showDebugInfo;
     }
 
     public boolean keyDown(int keyCode) {
-        if(keyCode == Input.Keys.ESCAPE) {
+        if (keyCode == Input.Keys.ESCAPE) {
             if (gameState.isPaused()) {
-                gameState.resume();    
+                gameState.resume();
             } else {
                 world.getCamera().velocityReset();
                 gameState.pause();
@@ -45,7 +37,7 @@ public class UIInputProcessor implements InputProcessor {
             world.getCamera().addVelocity(-1 * world.getCamera().vx, -1 * world.getCamera().vy);
             return true;
         }
-        if(gameState.isPaused()) {
+        if (gameState.isPaused()) {
             return true;
         }
         if (keyCode == Input.Keys.TAB) {
@@ -64,32 +56,39 @@ public class UIInputProcessor implements InputProcessor {
     }
 
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        if(gameState.isPaused()) {
+        if (gameState.isPaused()) {
             return true;
         }
-        Vector3 screenPos = viewport.getCamera().unproject(new Vector3(screenX, screenY, 0), viewport.getScreenX(), viewport.getScreenY(), viewport.getScreenWidth(), viewport.getScreenHeight());
+        Vector3 screenPos = buildingMenu.getViewport().getCamera().unproject(
+            new Vector3(screenX, screenY, 0),
+            buildingMenu.getViewport().getScreenX(),
+            buildingMenu.getViewport().getScreenY(),
+            buildingMenu.getViewport().getScreenWidth(),
+            buildingMenu.getViewport().getScreenHeight()
+        );
         if(screenPos.y < 100) {
             if(screenPos.y > 80) {
                 if(screenPos.x < 155) {
-                    currentMenuTab[0] = MenuTab.ACCOMMODATION;
+                    buildingMenu.setCurrentMenuTab(MenuTab.ACCOMMODATION);
                 } else if(screenPos.x > 245 && screenPos.x < 395) {
-                    currentMenuTab[0] = MenuTab.CATERING_RECREATION;
+                    buildingMenu.setCurrentMenuTab(MenuTab.CATERING_RECREATION);
                 } else if(screenPos.x > 490) {
-                    currentMenuTab[0] = MenuTab.TEACHING;
+                    buildingMenu.setCurrentMenuTab(MenuTab.TEACHING);
                 }
-                currentMenuItem[0] = -1;
+                buildingMenu.setCurrentMenuItem(-1);
                 world.setSelectedBuilding(null);
             } else if(screenPos.y < 75 && screenPos.y > 10){
                 int newItem = (int)((screenPos.x - 10) / 80);
-                if(newItem == currentMenuItem[0] || newItem > 5) {
-                    currentMenuItem[0] = -1;
+                if(newItem == buildingMenu.getCurrentMenuItem() || newItem > 5) {
+                    buildingMenu.setCurrentMenuItem(-1);
                     world.setSelectedBuilding(null);
                 } else {
-                    currentMenuItem[0] = newItem;
+                    buildingMenu.setCurrentMenuItem(newItem);
                 }
             }
-            if (currentMenuItem[0] >= 0 && currentMenuItem[0] < 5) {
-                VariantProperties variant = buildingVariants.get(currentMenuTab[0])[currentMenuItem[0]];
+            if (buildingMenu.getCurrentMenuItem() >= 0 && buildingMenu.getCurrentMenuItem() < 5) {
+                VariantProperties variant = buildingMenu.getBuildingVariants().get(
+                    buildingMenu.getCurrentMenuTab())[buildingMenu.getCurrentMenuItem()];
                 world.setSelectedBuilding(BuildingFactory.createBuilding(variant, world.currentGridPosition()));
             }
             return true;

22d18494d9ab21ec59e90e0ad6096f039a7d0055
james19321 <87946@farnborough.ac.uk>
2024-11-29 18:13:52 +0000

    Moved Trash.png to assets and deleted dir iconduck_assets. Improved formatting/spelling of code. Removed unused import statements. Removed VariantProperties currentVariant from DepthSortingTest. assertTrue replaced with assertEquals where possible.

--- src/test/java/y111studios/buildings/DepthSortingTest.java
+++ src/test/java/y111studios/buildings/DepthSortingTest.java
@@ -1,10 +1,9 @@
 package y111studios.buildings;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 import y111studios.GameState;
-import y111studios.Main;
 import y111studios.World;
 import y111studios.buildings.premade_variants.TeachingVariant;
 import y111studios.position.GridPosition;
@@ -19,12 +18,12 @@ public class DepthSortingTest {
     world.addObject(TeachingVariant.SUBJECT_HUB, new GridPosition(3, 3));
     world.addObject(TeachingVariant.SMALL_CLASSROOM, new GridPosition(6, 5));
 
-    assertTrue(world.getBuildings().get(0).getArea().getOrigin().equals(new GridPosition(6, 5)),
-      "Adding a building behind another building should result in it being sorted first");
+      assertEquals(world.getBuildings().get(0).getArea().getOrigin(), new GridPosition(6, 5),
+              "Adding a building behind another building should result in it being sorted first");
   }
 
   @Test
-  public void insertingInfront() {
+  public void insertingInFront() {
     GameState gameState = new GameState(75, 75);
     gameState.resume();
     World world = new World(null, gameState);
@@ -32,7 +31,7 @@ public class DepthSortingTest {
     world.addObject(TeachingVariant.SUBJECT_HUB, new GridPosition(3, 3));
     world.addObject(TeachingVariant.SMALL_CLASSROOM, new GridPosition(5, 6));
 
-    assertTrue(world.getBuildings().get(1).getArea().getOrigin().equals(new GridPosition(5, 6)),
-      "Adding a building infront of another building should result in it being sorted second");
+      assertEquals(world.getBuildings().get(1).getArea().getOrigin(), new GridPosition(5, 6),
+              "Adding a building in front of another building should result in it being sorted second");
   }
 }

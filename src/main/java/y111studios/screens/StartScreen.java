7efee998f7b6053b0e673695a7ba32bd020af191
Bertie Cartwright <sunstonesharptor@gmail.com>
2024-12-02 18:26:56 +0000

    Fixed stretching of start screen background

--- src/main/java/y111studios/screens/StartScreen.java
+++ src/main/java/y111studios/screens/StartScreen.java
@@ -45,9 +45,8 @@ public class StartScreen extends ScreenAdapter {
     this.game = game;
     viewport = new ScreenViewport();
     camera = (OrthographicCamera) viewport.getCamera();
-    camera.setToOrtho(false, width, height);
     startScreen = game.assetLib.manager.get(AssetPaths.START_SCREEN.getPath());
-    
+
     stage = new Stage(viewport);
     createMenu();
   }
@@ -108,10 +107,13 @@ public class StartScreen extends ScreenAdapter {
 
     game.spritebatch.begin();
     //game.spritebatch.setColor(new Color(0.0f, 0.0f, 0.0f, 0.1f));
+    float backgroundWidth = (float)viewport.getScreenWidth() / viewport.getScreenHeight()
+      * startScreen.getHeight();
     game.spritebatch.draw(
         startScreen,
         0, 0, viewport.getScreenWidth(), viewport.getScreenHeight(),
-        0, 0, startScreen.getWidth(), startScreen.getHeight(),
+        (int)(startScreen.getWidth() / 2.0 - backgroundWidth / 2.0), 0,
+        (int)backgroundWidth, startScreen.getHeight(),
         false, false);
     game.spritebatch.end();
 

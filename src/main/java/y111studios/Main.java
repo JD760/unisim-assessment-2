393b27582fe0c51ca1c7d00ae041e4a1daa6ac22
Bertie Cartwright <sunstonesharptor@gmail.com>
2024-12-06 21:26:34 +0000

    Added background music

--- src/main/java/y111studios/Main.java
+++ src/main/java/y111studios/Main.java
@@ -1,5 +1,7 @@
 package y111studios;
 
+import com.badlogic.gdx.Audio;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
@@ -15,6 +17,7 @@ public class Main extends Game {
   public AssetLibrary assetLib;
   public SpriteBatch spritebatch;
   public BitmapFont font;
+  Music backgroundMusic;
 
   @Override
   public void create() {
@@ -26,6 +29,12 @@ public class Main extends Game {
     font.getData().setScale(1.3f);
     Gdx.graphics.setWindowedMode(1280, 720);
     this.setScreen(new StartScreen(this));
+
+    Audio audio = Gdx.audio;
+    backgroundMusic = audio.newMusic(Gdx.files.internal("assets/retro-8bit-happy-videogame-music-243998.mp3"));
+    backgroundMusic.setVolume(0.5f);
+    backgroundMusic.setLooping(true);
+    backgroundMusic.play();
   }
 
   /**

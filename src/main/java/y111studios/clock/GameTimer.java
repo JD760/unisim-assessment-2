b16cbe369cf132f54b5cb3e7c6fd8247173f2c0d
Bertie Cartwright <sunstonesharptor@gmail.com>
2025-01-09 10:53:15 +0000

    Fixed the asset path for one of the snowy trees and changed the timer to last for 5 minutes

--- src/main/java/y111studios/clock/GameTimer.java
+++ src/main/java/y111studios/clock/GameTimer.java
@@ -13,7 +13,7 @@ public interface GameTimer {
   /**
    * The maximum duration of the timer in seconds.
    */
-  int INITIAL_SECONDS = 60;
+  int INITIAL_SECONDS = 300;
   /**
    * The maximum duration of the timer.
    */
